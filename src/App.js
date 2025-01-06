1e9b1861d7ff731bd34c2d91aa29d6aee3a0cd54
Arita <aritasam98@gmail.com>
2025-01-06 22:45:04 +0000

    chore: Added a landing page template that'll be edited to fit the specifics of the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-8xl text-black-50 italic">
+      {/* <h1 className="text-8xl text-black-50">
        Zaka
-      </h1>
+      </h1> */}
+      <Header />
     </div>
   );
 }
