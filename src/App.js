d7438bd4a2611ec2110ce627b49e05bd5ac47781
Samuel <aritasam98@gmail.com>
2025-01-14 14:33:55 +0300

    code: Added the landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
+import Header from './components/header/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-8xl text-black-50 italic">
-       Zaka
-      </h1>
+     
+      <Header />
     </div>
   );
 }
