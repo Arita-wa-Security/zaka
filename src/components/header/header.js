60fb9b39919f177c1c5ea2be3bed66d10d7c9c49
Samuel <aritasam98@gmail.com>
2025-01-15 13:55:52 +0300

    fix: Epanded the image container

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,14 +5,14 @@ function Header(){
     return (
       <body className="sm:font-poppins sm:bg-white relative">
         <div
-          id="container"
+          id="container_1"
           className="p-20 w-auto flex px-24 justify-center relative"
         >
           <div
             id="container"
             className="p-20 sm:p-16 md:p-20 lg:p-24 xl:p-20 w-auto flex flex-col md:flex-row px-4 sm:px-8 md:px-24 lg:px-24 xl:px-24 relative"
           >
-            <div className="mr-10 sm:w-16 md:w-20 lg:w-24 xl:w-20">
+            <div className="mr-10">
               {/* <img
                    className="rounded-lg min-w-[100px] w-full h-auto md:w-auto md:h-auto"
                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMwjitFKJGaGoMmOhT8TxWhH4ML9v5yvojog&s"
